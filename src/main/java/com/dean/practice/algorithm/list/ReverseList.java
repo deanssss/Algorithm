32d0aaff9051e4d3ad347d62bbd0065b12074353
deanssss <deanssss520@gmail.com>
2022-03-08 00:09:36 +0800

    链表翻转TopN， 链表翻转MN。

--- src/main/java/com/dean/practice/algorithm/list/ReverseList.java
+++ src/main/java/com/dean/practice/algorithm/list/ReverseList.java
@@ -17,11 +17,45 @@ public class ReverseList {
         return head;
     }
 
-//    public ListNode reverseList(ListNode head) {
-//        if (head == null || head.next == null)
-//            return head;
-//
-//        ListNode newHead = reverseList(head.next);
-//        head.next.next = head
-//    }
+    public ListNode reverseList(ListNode head) {
+        // 不要跳进递归里，一层递归做一层递归的事，然后处理好终止条件。
+        // 1 -> 2 -> 3 -> 4 -> null
+        // 1 -> reverse(2 -> 3 -> 4 -> null) 假设已经拿到下一层递归的结果了
+        // 1 -> 2 <- 3 <- 4
+        //      |
+        //     null
+        // null <- 1 <- 2 <- 3 <- 4
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    private ListNode successor = null;
+    // 简化题目，默认n <= 链表长度
+    public ListNode reverseListN(ListNode head, int n) {
+        if (head == null) return null;
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+
+        ListNode newHead = reverseListN(head.next, --n);
+        head.next.next = head;
+        head.next = successor;
+        return newHead;
+    }
+
+    // 0< m <= n <= list.length
+    // https://leetcode-cn.com/problems/reverse-linked-list-ii/solution/yi-bu-yi-bu-jiao-ni-ru-he-yong-di-gui-si-lowt/
+    public ListNode reverseListMN(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseListN(head, n);
+        }
+        head.next = reverseListMN(head.next, m - 1, n - 1);
+        return head;
+    }
 }
--- src/test/java/com/dean/practice/algorithm/list/TestReverseList.kt
+++ src/test/java/com/dean/practice/algorithm/list/TestReverseList.kt
@@ -5,26 +5,39 @@ import org.junit.Test
 class TestReverseList {
     private val reverse = ReverseList()
 
-    @Test fun  test_reverseList1() {
-        val cases = listOf(
-            TestCase(
-                input = listOf(1, 2, 3, 4, 5),
-                output = listOf(5, 4, 3, 2, 1)
-            ),
-            TestCase(
-                input = listOf(1, 2),
-                output = listOf(2, 1)
-            ),
-            TestCase(
-                input = listOf(2),
-                output = listOf(2)
-            ),
-            TestCase(
-                input = listOf(),
-                output = listOf()
-            )
-        )
+    private val cases = listOf(
+        TestCase(
+            input = listOf(1, 2, 3, 4, 5),
+            output = listOf(5, 4, 3, 2, 1)
+        ),
+        TestCase(
+            input = listOf(1, 2),
+            output = listOf(2, 1)
+        ),
+        TestCase(
+            input = listOf(2),
+            output = listOf(2)
+        ),
+        TestCase(
+            input = listOf(),
+            output = listOf()
+        ),
+    )
 
+    @Test fun  test_reverseList() {
+        cases.map {
+            val head = ListUtil.buildFromValues(it.input)
+            val res = reverse.reverseList(head)
+            println("Input:" + it.input.toString())
+            println("Expect:" + it.output.toString())
+            val reStr = ListUtil.parseListToArrayStr(res)
+            println("Result:$reStr")
+            println("==========================")
+            assert(it.output.toString() == reStr)
+        }
+    }
+
+    @Test fun  test_reverseList1() {
         cases.map {
             val head = ListUtil.buildFromValues(it.input)
             val res = reverse.reverseList1(head)
@@ -37,8 +50,79 @@ class TestReverseList {
         }
     }
 
+    @Test fun test_reverseListN() {
+        listOf(
+            TestCaseN(
+                listOf(1, 2, 3, 4, 5),
+                3,
+                listOf(3, 2, 1, 4, 5)
+            ),
+            TestCaseN(
+                listOf(1, 2, 3, 4, 5),
+                5,
+                listOf(5, 4, 3, 2, 1)
+            ),
+            TestCaseN(
+                listOf(1, 2, 3, 4, 5),
+                1,
+                listOf(1, 2, 3, 4, 5)
+            ),
+            TestCaseN(
+                listOf(1),
+                1,
+                listOf(1)
+            ),
+            TestCaseN(
+                listOf(),
+                0,
+                listOf()
+            ),
+        ).map {
+            val head = ListUtil.buildFromValues(it.input)
+            val res = reverse.reverseListN(head, it.n)
+            println("Input:${it.input} n=${it.n}")
+            println("Expect:${it.output}")
+            val reStr = ListUtil.parseListToArrayStr(res)
+            println("Result:$reStr")
+            println("==========================")
+            assert(it.output.toString() == reStr)
+        }
+    }
+
+    @Test fun test_reverseListMN() {
+        listOf(
+            TestCaseMN(
+                listOf(1, 2, 3, 4, 5, 6),
+                3, 5,
+                listOf(1, 2, 5, 4, 3, 6),
+            )
+        ).map {
+            val head = ListUtil.buildFromValues(it.input)
+            val res = reverse.reverseListMN(head, it.m, it.n)
+            println("Input:${it.input} m=${it.m} n=${it.n}")
+            println("Expect:${it.output}")
+            val reStr = ListUtil.parseListToArrayStr(res)
+            println("Result:$reStr")
+            println("==========================")
+            assert(it.output.toString() == reStr)
+        }
+    }
+
     private data class TestCase(
         val input: List<Int>,
+        val output: List<Int>
+    )
+
+    private data class TestCaseN(
+        val input: List<Int>,
+        val n: Int,
+        val output: List<Int>
+    )
+
+    private data class TestCaseMN(
+        val input: List<Int>,
+        val m: Int,
+        val n: Int,
         val output: List<Int>
     )
 }
